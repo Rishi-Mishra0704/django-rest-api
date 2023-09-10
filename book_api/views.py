1a72ef8931ef587b639ae129fb7720b4c8af520b
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-10 23:01:17 +0530

    get request!!!

--- book_api/views.py
+++ book_api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .serializer import BookSerializer
 from .models import Book
 
 # Create your views here.
-
+@api_view(['GET'])
 def book_list(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
     

1cf4759d4b5cc427630cbff80d6a2943c54f3e43
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-10 22:45:57 +0530

    Add model and view

--- book_api/views.py
+++ book_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Book
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    display_books = list(books.values)
+    return JsonResponse({
+        "books": display_books
+    })

d5c5fb8d24483ec8d6bc010b18a35bf9ae86b729
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-11 17:54:41 +0530

    add serializer and migrations

---
+++ movie_api/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Movie
+from .models import Actor
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['id', 'name', 'age']
+
+
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description', 'release_date', 'actors']
--- restApi/settings.py
+++ restApi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movie_api',
     'book_api',
     'rest_framework',
     'django.contrib.admin',

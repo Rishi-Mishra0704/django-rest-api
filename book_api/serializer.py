56390439b0c81537bfd8ee791b61372137b46476
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-11 17:37:01 +0530

    Update serializers

--- book_api/serializer.py
+++ book_api/serializer.py
@@ -6,14 +6,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'price')
-
-    def create(self, data):
-        return Book.objects.create(**data)
-    
-    def update(self, instance, data):
-        instance.title = data.get('title', instance.title)
-        instance.author = data.get('author', instance.author)
-        instance.price = data.get('price', instance.price)
-        instance.save()
-        return instance
+        fields = '__all__'

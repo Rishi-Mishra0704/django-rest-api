1890e68f2fadf35e0d9a21cf3d08c66421d10ced
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-11 18:51:25 +0530

    Add actor get and post methods

--- movie_api/views.py
+++ movie_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Actor, Movie
+from .serializer import ActorSerializer, MovieSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def actor_list(request):
+    actors = Actor.objects.all()
+    serializer = ActorSerializer(actors, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def actor_create(request):
+    serializer = ActorSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
